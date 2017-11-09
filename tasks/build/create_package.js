da97ac35b4c672416dc267d6a4d7653cada4eb6e
Joe Fleming <joe.fleming@gmail.com>
2017-11-09 10:00:45 -0700

    fix: pass build target into package task
    use the build target for the zip output

--- tasks/build/create_package.js
+++ tasks/build/create_package.js
@@ -4,9 +4,8 @@ const del = require('del');
 const vfs = require('vinyl-fs');
 const zip = require('gulp-zip');
 
-module.exports = function createPackage(plugin, buildVersion) {
+module.exports = function createPackage(plugin, buildTarget, buildVersion) {
   const buildId = `${plugin.id}-${buildVersion}`;
-  const buildTarget = join(plugin.root, 'build');
   const buildRoot = join(buildTarget, 'kibana', plugin.id);
 
   // zip up the package
