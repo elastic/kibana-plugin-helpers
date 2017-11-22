76280374a8e4c2222feb40ca60536c7ccf455a54
Jonathan Budzenski <jon@jbudz.me>
2017-11-22 16:48:25 -0600

    Split node options

--- tasks/start/start_action.js
+++ tasks/start/start_action.js
@@ -1,14 +1,15 @@
 const execFileSync = require('child_process').execFileSync;
 const  { join } = require('path');
+const split = require('argv-split')
 
 module.exports = function (plugin, run, options) {
   options = options || {};
 
   const cmd = 'node';
   const script = join('scripts', 'kibana.js');
-  const nodeOptions = process.env.NODE_OPTIONS;
+  const nodeOptions = split(process.env.NODE_OPTIONS || '');
 
-  let args = [nodeOptions, script, '--dev', '--plugin-path', plugin.root].filter(Boolean);
+  let args = nodeOptions.concat([script, '--dev', '--plugin-path', plugin.root]);
 
   if (Array.isArray(plugin.includePlugins)) {
     plugin.includePlugins.forEach((path) => {
